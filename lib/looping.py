3138bfc736a467a78dd9a8cfde46be25e853f1ff
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-09-26 00:59:37 -0400

    Finished Lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10):
+        print(10 - i)
+        print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for num in int_list:
+        squared_list.append(num ** 2)
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
